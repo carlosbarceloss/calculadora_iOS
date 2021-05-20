9185a41b2d34d5f1286755f51c1ac64a2a7b3215
Carlos Barcelos <carlosbarcelos@rede.ulbra.br>
2021-05-19 21:22:08 -0300

    feat - create main view
    created StackView structure to replicate iOS calculator

---
+++ Calculadora/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Calculadora
+//
+//  Created by Carlos Barcelos on 18/05/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
